0b4db45069f6bc33631405c4debdff15494c4e2a
itsakarya <aniket2018pr@gmail.com>
2024-12-19 03:46:01 +0530

    sigin/signup setup using clerk

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,9 @@
-import { Geist, Geist_Mono, Inter } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/header";
+import { ClerkProvider } from "@clerk/nextjs";
 
-const inter = Inter({subsets: ['latin']})
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Welth",
@@ -10,12 +12,20 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+    <ClerkProvider>
     <html lang="en">
-      <body
-        className={`${inter.className}`}
-      >
-        {children}
+      <body className={`${inter.className}`}>
+        {/* header */}
+        <Header />
+        <main className="min-h-screen">{children}</main>
+        {/* footer */}
+        <footer className="bg-blue-100 py-10">
+          <div className="container mx-auto px-4 text-center text-gray-500">
+            I&apos;m a chill guy
+          </div>
+        </footer>
       </body>
     </html>
+    </ClerkProvider>
   );
 }
